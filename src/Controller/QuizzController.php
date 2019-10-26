693733da3b2ce60e95c49c439d1cdedbff77780f
Stephane <lecoutrestephane@outlook.com>
2019-10-26 16:25:19 +0200

    jeu du quiz

---
+++ src/Controller/QuizzController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuizzController extends AbstractController
+{
+    /**
+     * @Route("/quizz", name="quizz")
+     */
+    public function index()
+    {
+        return $this->render('quizz/index.html.twig');
+    }
+}
