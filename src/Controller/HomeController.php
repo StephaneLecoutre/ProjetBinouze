c7e010e6de41ee4382975c57678976230011281b
annedeb <annedeb2019@gmail.com>
2019-11-05 09:47:50 +0100

    add use controlleer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Twig\Environment;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
